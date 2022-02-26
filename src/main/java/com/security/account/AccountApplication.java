fd486a7163299c11b3d2e88f4817072867a82689
Elias Borges <eliasborges@unipam.edu.br>
2022-02-26 12:37:52 -0300

    Adição do logs na aplicação

--- src/main/java/com/security/account/AccountApplication.java
+++ src/main/java/com/security/account/AccountApplication.java
@@ -1,13 +1,20 @@
 package com.security.account;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@Slf4j
 public class AccountApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(AccountApplication.class, args);
+
+		log.info("\n------------------------\n"
+				+ "    Security Account\n"
+				+ "------------------------"
+		);
 	}
 
 }
